550e4657a128e96ad101cd20f0edd7927a200b41
Hikari-9527 <2752345532@qq.com>
2026-07-26 16:59:48 +0800

    Revert "提交代码"
    This reverts commit 721492697976f8b12a9271feab662831a2955014, reversing
    changes made to 36367f1d7a2504d57ec726eea91973743c0ca189.

--- aitutor-backend-java/src/main/java/com/treepeople/leapmindtts/mapper/UserTeacherPreferenceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.treepeople.leapmindtts.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.treepeople.leapmindtts.pojo.entity.UserTeacherPreference;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserTeacherPreferenceMapper extends BaseMapper<UserTeacherPreference> {
-}
